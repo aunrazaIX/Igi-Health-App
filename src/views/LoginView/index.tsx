ce9c8466c8133aeb1f5c90a7b033787872ffac1b
Muhammad Usman <muhammad.usman@integrationxperts.com>
2025-05-13 13:03:53 +0500

    merged with dev-hariss

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -1,10 +1,4 @@
-import {
-  View,
-  ImageBackground,
-  Image,
-  TouchableOpacity,
-  Text,
-} from 'react-native';
+import { View, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import React, { JSX } from 'react';
 import { images, icons } from '../../assets';
 import { styles } from './styles';
@@ -13,50 +7,54 @@ import LinearGradient from 'react-native-linear-gradient';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { AileronSemiBold, CurvedView } from '../../components';
 import { LoginForm, SignUpView } from './components';
-import { User } from '../../viewmodels/useLoginViewModel'
-
+import { User } from '../../viewmodels/useLoginViewModel';
+import { vh } from '../../assets/theme/dimension';
 
 const LoginView = ({
   onPressTab,
   selectedTab,
   tabs,
   user,
-  onPress,
-
-
+  onPressforgotPassword,
+  handleChange,
+  handleLogin,
+  loading,
 }: {
   onPressTab: (name: string) => void;
   selectedTab: string;
   tabs: string[];
-  user: User,
-  onPress: (to: string) => void,
-  setuser: React.Dispatch<React.SetStateAction<User>>
+  user: User;
+  onPressforgotPassword: (to: string) => void;
+  handleChange: (field: keyof User, value: string) => void;
+  handleLogin: () => void;
+  loading: boolean;
 }) => {
-
-
-
-
   const Wrapper = (tab: string) =>
     selectedTab === tab ? LinearGradient : TouchableOpacity;
 
   const renderForm: Record<string, JSX.Element> = {
-    login: <LoginForm onPress={onPress} />,
-    signup: <SignUpView onPress={onPress} onPressTab={onPressTab} />,
+    login: (
+      <LoginForm
+        handleChange={handleChange}
+        user={user}
+        onPressforgotPassword={onPressforgotPassword}
+        handleLogin={handleLogin}
+        loading={loading}
+      />
+    ),
+    signup: <SignUpView />,
   };
   return (
-
-    <KeyboardAwareScrollView  >
-
+    <KeyboardAwareScrollView enableOnAndroid>
       <ImageBackground
         source={images.loginBackground}
         style={styles.imageContainer}>
-
-
         <View style={styles.loginContent}>
           <Image source={icons.logo} style={styles.logo} />
 
-          <CurvedView containerStyle={styles.curvedStyle}>
-
+          <CurvedView
+            containerStyle={styles.curvedStyle}
+            backColor={{ backgroundColor: 'transparent' }}>
             <View style={styles.loginContainer}>
               <View style={styles.tabContainer}>
                 {tabs.map(tab => {
@@ -66,21 +64,22 @@ const LoginView = ({
                       key={tab}
                       onPress={() => onPressTab(tab)}
                       style={styles.tab}
-                      colors={COLORS.PriorGradient}
-                    >
-                      <AileronSemiBold
-                        style={[
-                          styles.tabText,
-                          {
-                            color:
-                              selectedTab === tab
-                                ? COLORS.white
-                                : COLORS.textColor,
-                          },
-                        ]}
-                        numberOfLines={2}
-                        name={tab === 'login' ? 'Login' : 'Signup'}
-                      />
+                      colors={COLORS.PriorGradient}>
+                      <View style={styles.wrapper}>
+                        <AileronSemiBold
+                          style={[
+                            styles.tabText,
+                            {
+                              color:
+                                selectedTab === tab
+                                  ? COLORS.white
+                                  : COLORS.textColor,
+                            },
+                          ]}
+                          numberOfLines={2}
+                          name={tab === 'login' ? 'Login' : 'Signup'}
+                        />
+                      </View>
                     </TabWrapper>
                   );
                 })}
@@ -88,14 +87,12 @@ const LoginView = ({
 
               {renderForm[selectedTab]}
             </View>
-
           </CurvedView>
         </View>
-
       </ImageBackground>
     </KeyboardAwareScrollView>
-
   );
 };
 
 export default LoginView;
+
