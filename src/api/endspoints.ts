c354e2a4e42161c9b892a87e629574b014fb3253
Aun Raza <aun.raza@integrationxperts.com>
2025-05-15 12:03:50 +0500

    implemented claimHistory api

--- src/api/endspoints.ts
+++ src/api/endspoints.ts
@@ -10,6 +10,9 @@ const endpoints = {
     attachment: (userId: string, myuuid: string, ClientCode: string) =>
       `Attachment/upload?userId=${userId}&UUID=${myuuid}&ClientCode=${ClientCode}`,
   },
+  claimHistory: {
+    getAllClaim: 'Claims/getClaimsView',
+  },
 };
 
 export default endpoints;
