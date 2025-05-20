1f202755ee9596cd4039867fc8d4647aec411c6f
Muhammad <muhammad.hariss@integrationxperts.com>
2025-05-20 14:57:22 +0500

    working on lodge

--- src/redux/lodgeSlice.ts
+++ src/redux/lodgeSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice, current } from '@reduxjs/toolkit';
-import { clampRGBA } from 'react-native-reanimated/lib/typescript/Colors';
+import {createAsyncThunk, createSlice, current} from '@reduxjs/toolkit';
+import {clampRGBA} from 'react-native-reanimated/lib/typescript/Colors';
+import {setErrorModal} from './generalSlice';
 
 const initialState: {
   currentStep: number;
@@ -12,71 +13,63 @@ const initialState: {
   selectedPatient: null,
   selectedDocuments: [],
   treatments: [],
-  isError: false
+  isError: false,
 };
 
-
+export const updateTreatments = createAsyncThunk(
+  'lodge/update',
+  async (_data, thunkApi) => {
+    const {index, data} = _data;
+    const {treatments} = thunkApi.getState()?.lodge;
+    const isDuplicate = treatments?.some(
+      treatment => treatment?.receiptNumber == data?.receiptNumber,
+    );
+    if (isDuplicate) {
+      thunkApi.dispatch(setErrorModal({message: 'Record Already Exist'}));
+    } else {
+      thunkApi.dispatch(_updateTreatmentData({index, data}));
+    }
+  },
+);
 
 export const lodgeSlice = createSlice({
   name: 'lodgeSlice',
   initialState,
   reducers: {
-    setSelectedPatient: (state, { payload }) => {
+    setSelectedPatient: (state, {payload}) => {
       state.selectedPatient = payload;
     },
 
     setTreatments: (state, action) => {
-
       if (action?.payload?.length > 0) {
         const existingTreatments = current(state.treatments);
 
-        const newTreatments = action.payload.filter((newItem) => {
-          const isDuplicate = existingTreatments.some((existingItem) =>
-            existingItem.treatment?.value === newItem.treatment?.value &&
-            existingItem.receiptNumber === newItem.receiptNumber &&
-            existingItem.amount === newItem.amount &&
-            existingItem.description === newItem.description
+        const newTreatments = action.payload.filter(newItem => {
+          const isDuplicate = existingTreatments.some(
+            existingItem =>
+              existingItem.treatment?.value === newItem.treatment?.value &&
+              existingItem.receiptNumber === newItem.receiptNumber &&
+              existingItem.amount === newItem.amount &&
+              existingItem.description === newItem.description,
           );
 
           if (isDuplicate) {
             state.isError = true;
           }
 
-
           return !isDuplicate;
-
         });
 
         state.treatments = [...state.treatments, ...newTreatments];
-
       } else {
         state.treatments = [];
       }
     },
 
-
-
-    updateTreatments: (state, { payload }) => {
-      const { index, data } = payload;
-      const existingTreatments = current(state.treatments);
-
-      const isDuplicate = existingTreatments.some((item, i) =>
-        i !== index &&
-        item.treatment?.value === data.treatment?.value &&
-        item.receiptNumber === data.receiptNumber &&
-        item.amount === data.amount &&
-        item.description === data.description
-      );
-
-      if (isDuplicate) {
-        state.isError = true;
-      } else {
-        state.treatments[index] = data;
-      }
+    _updateTreatmentData: (state, {payload}) => {
+      const {index, data} = payload;
+      state.treatments[index] = data;
     },
-
-
-
 
     setStep: (state, action) => {
       state.currentStep = action.payload;
@@ -92,21 +85,18 @@ export const lodgeSlice = createSlice({
       }
     },
 
-
-    onDeleteTreatment: (state, { payload }) => {
+    onDeleteTreatment: (state, {payload}) => {
       let temp = [...state.treatments];
       temp.splice(payload, 1);
       state.treatments = temp;
     },
-
-
   },
 });
 
 export const {
   setSelectedPatient,
   setTreatments,
-  updateTreatments,
+  _updateTreatmentData,
   setStep,
   setSelectedDocuments,
   onDeleteTreatment,
