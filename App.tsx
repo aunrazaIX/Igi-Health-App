cead12b7458823def586865a052391150bda0cfc
Muhammad <muhammad.hariss@integrationxperts.com>
2025-06-26 12:22:32 +0500

    test

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainStack from './src/navigation/MainStack';
-import {StatusBar, View} from 'react-native';
+import {Platform, SafeAreaView, StatusBar, View} from 'react-native';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {Provider, useSelector} from 'react-redux';
 import {persistor, store} from './src/redux/store';
@@ -20,14 +20,22 @@ const App = () => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer theme={MyTheme}>
-          <StatusBar
-            barStyle={'dark-content'}
-            backgroundColor={COLORS.loginContainer}
-          />
-          <MainStack />
-          <View>
-            <ErrorModal />
-          </View>
+          <SafeAreaView
+            style={{
+              flex: 1,
+              backgroundColor: COLORS.loginContainer,
+              paddingTop:
+                Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+            }}>
+            <StatusBar
+              barStyle={'dark-content'}
+              backgroundColor={COLORS.loginContainer}
+            />
+            <MainStack />
+            <View>
+              <ErrorModal />
+            </View>
+          </SafeAreaView>
         </NavigationContainer>
       </PersistGate>
     </Provider>
