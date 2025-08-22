c156a9a7a6ab5a626d68ecc771093df6a0f702b7
Hariss <harissbilal@hotmail.com>
2025-08-22 17:43:57 +0500

    populated data

--- src/viewmodels/usePriorApprovalHistoryViewModel.js
+++ src/viewmodels/usePriorApprovalHistoryViewModel.js
@@ -10,113 +10,80 @@ import {formatCurrencyWithPKR, formatName} from '../utils';
 const usePriorApprovalHistoryViewModel = () => {
   const {user} = useSelector(state => state.auth);
   const navigation = useNavigation();
-  const [type, setType] = useState(
-    user?.coverageType?.some(obj => obj?.isAllowed !== true)
-      ? 'Processed'
-      : 'In-Process',
-  );
   const [data, setData] = useState([]);
   const [showRemarks, setShowRemarks] = useState(false);
   const [remarks, setRemarks] = useState('');
 
-  const isInProcessAllowed = user?.showPriorApproval;
-
   const goBack = () => navigation.goBack();
   const onCloseRemarksModal = () => setShowRemarks(false);
 
-  const getHeadingSubHeading = useMemo(() => {
-    return {
-      'In-Process': {
-        heading: 'In-Process Prior Approvals',
-        messsage:
-          'Prior Approvals initiated via the app or portal that are currently being evaluated.',
-      },
-      Processed: {
-        heading: 'Processed Prior Approvals',
-        messsage:
-          'All finalized Prior Approvals, including hospital visits, reimbursements, rejections and portal submissions.',
-      },
-    };
-  }, []);
-
   const transformClaimData = (claim, isInProcess) => ({
-    headerLabel: `Claim #${claim.RequestID}`,
-    ClaimStatus: claim.RequestSatus,
+    headerLabel: `Prior Approval #${claim.RequestID}`,
+    ClaimStatus: claim.RequestStatus,
     headerIcon: icons.taskEdit,
     RelationName: claim.RelationName,
     items: [
       {
         label: 'Patient Name:',
-        value: claim.UserRelationName
-          ? formatName(claim.UserRelationName.trim())
+        value: claim?.UserRelationName
+          ? formatName(claim?.UserRelationName.trim())
           : '--',
       },
-      claim.RequestAddedDateTime && {
-        label: 'Admission/Procedure Date:',
-        value: moment(
-          claim.RequestAddedDateTime,
-          isInProcess ? 'YYYY-MMM-DD' : 'YYYY-MM-DD',
-        ).format('DD-MMM-YYYY'),
-      },
-      {label: 'Treatment Type:', value: claim?.TreatmentTypeName},
+      {label: 'Relationship:', value: claim?.relation_type?.trim()},
       {
         label: 'Hospital Name:',
         value: claim?.HospitalName,
       },
-      {label: 'Status:', value: claim.RequestStatus},
-      (isInProcess ? claim.ClaimsDescription : claim.DeductionReason) && {
-        label: 'Claim Remarks:',
-        value: 'View Remarks',
-        isUnderLine: true,
-        onPress: () => {
-          setShowRemarks(true);
-          setRemarks(
-            isInProcess ? claim.ClaimsDescription : claim.DeductionReason,
-          );
-        },
+      {label: 'Treatment/Service:', value: claim?.TreatmentTypeName?.trim()},
+      {label: 'Admission/M.R.#:', value: claim?.admission_number ?? '--'},
+      {
+        label: 'Admission/Procedure Date:',
+        value: claim?.admission_date
+          ? moment(claim?.admission_date).format('DD-MMM-YYYY')
+          : '--',
+      },
+      {label: 'Estimated Cost:', value: formatCurrencyWithPKR(claim?.Amount)},
+      {label: 'Description:', value: claim?.RequestComments},
+      {label: 'Status:', value: claim?.RequestStatus},
+      {label: 'Request ID:', value: claim?.RequestID},
+      {
+        label: 'Request Date:',
+        value: moment(claim?.RequestAddedDateTime).format(
+          'MMM DD,YYYY hh:mm A',
+        ),
+      },
+      {
+        label: 'Closure Date:',
+        value: claim?.ClosureDate
+          ? moment(claim?.ClosureDate).format('MMM DD,YYYY hh:mm A')
+          : '--',
+      },
+      {
+        label: 'Approved Center Remarks:',
+        value: claim?.request_closed_remarks || '--',
       },
     ].filter(Boolean),
   });
 
-  const {loading: dxcClaimLoading, trigger: getDxcClaims} = useApiHook({
-    apiEndpoint: endpoints.claimHistory.getDxcClaims,
-    method: 'get',
-    argsOrBody: {userid: user?.UserId},
-    skip: !isInProcessAllowed,
-    onSuccess: res => {
-      setData(res?.Data?.map(claim => transformClaimData(claim, false)));
-    },
-  });
-
-  const {loading: claimDataLoading, trigger: geInProcessClaims} = useApiHook({
+  const {loading} = useApiHook({
     apiEndpoint: endpoints.PriorApprovalHistory.getPriorApprovalRequests,
     method: 'get',
-    argsOrBody: {userid: user?.UserId},
-    skip: isInProcessAllowed,
+    argsOrBody: {userId: user?.UserId, clientCode: user?.ClientCode},
     onSuccess: res => {
-      setData(res?.Data?.map(claim => transformClaimData(claim, true)));
+      console.log(res, 'res');
+      setData(res?.map(claim => transformClaimData(claim, true)));
     },
   });
-
-  const onPressType = _type => {
-    setType(_type);
-    setData([]);
-    _type === 'Processed' ? getDxcClaims() : geInProcessClaims();
-  };
 
   return {
     states: {
       data,
-      claimDataLoading: claimDataLoading || dxcClaimLoading,
-      type,
+      claimDataLoading: loading,
       showRemarks,
       remarks,
-      getHeadingSubHeading,
-      isInProcessAllowed,
     },
     functions: {
       goBack,
-      onPressType,
       onCloseRemarksModal,
     },
   };
